c7184485bf4cf94b72f20beea22fba13ada63c4c
meveo.admin <no-mail@meveo.com>
2022-02-23 10:06:51 +0000

    Add the cet java source file : Credential.javain the module : mv-credentials

--- facets/java/org/meveo/model/customEntities/Credential.java
+++ facets/java/org/meveo/model/customEntities/Credential.java
@@ -30,8 +30,6 @@ public class Credential implements CustomEntity {
 
     private String status;
 
-    private String token;
-
     @JsonProperty(required = true)
     private String username;
 
@@ -90,14 +88,6 @@ public class Credential implements CustomEntity {
 
     public void setStatus(String status) {
         this.status = status;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
     }
 
     public String getUsername() {
