31273fdd26ba57039737790104fbfda1a0c28f56
meveo.admin <no-mail@meveo.com>
2022-02-23 10:05:06 +0000

    Add the cet java source file : Credential.javain the module : mv-credentials

--- facets/java/org/meveo/model/customEntities/Credential.java
+++ facets/java/org/meveo/model/customEntities/Credential.java
@@ -3,6 +3,8 @@ package org.meveo.model.customEntities;
 import org.meveo.model.CustomEntity;
 import java.util.List;
 import org.meveo.model.persistence.DBStorageType;
+import java.time.Instant;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public class Credential implements CustomEntity {
@@ -19,6 +21,39 @@ public class Credential implements CustomEntity {
     @JsonIgnore()
     private DBStorageType storages;
 
+    private String apiKey;
+
+    private String headerValue;
+
+    private Instant tokenExpiry;
+
+    private String publicKey;
+
+    private Instant lastConnection;
+
+    private String token;
+
+    private String privateKey;
+
+    private String password;
+
+    private String domainName;
+
+    private String passwordSecret;
+
+    private String authenticationType;
+
+    private String headerKey;
+
+    private Long credit;
+
+    private String refreshToken;
+
+    private String status;
+
+    @JsonProperty(required = true)
+    private String username;
+
     @Override()
     public String getUuid() {
         return uuid;
@@ -34,6 +69,134 @@ public class Credential implements CustomEntity {
 
     public void setStorages(DBStorageType storages) {
         this.storages = storages;
+    }
+
+    public String getApiKey() {
+        return apiKey;
+    }
+
+    public void setApiKey(String apiKey) {
+        this.apiKey = apiKey;
+    }
+
+    public String getHeaderValue() {
+        return headerValue;
+    }
+
+    public void setHeaderValue(String headerValue) {
+        this.headerValue = headerValue;
+    }
+
+    public Instant getTokenExpiry() {
+        return tokenExpiry;
+    }
+
+    public void setTokenExpiry(Instant tokenExpiry) {
+        this.tokenExpiry = tokenExpiry;
+    }
+
+    public String getPublicKey() {
+        return publicKey;
+    }
+
+    public void setPublicKey(String publicKey) {
+        this.publicKey = publicKey;
+    }
+
+    public Instant getLastConnection() {
+        return lastConnection;
+    }
+
+    public void setLastConnection(Instant lastConnection) {
+        this.lastConnection = lastConnection;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getPrivateKey() {
+        return privateKey;
+    }
+
+    public void setPrivateKey(String privateKey) {
+        this.privateKey = privateKey;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDomainName() {
+        return domainName;
+    }
+
+    public void setDomainName(String domainName) {
+        this.domainName = domainName;
+    }
+
+    public String getPasswordSecret() {
+        return passwordSecret;
+    }
+
+    public void setPasswordSecret(String passwordSecret) {
+        this.passwordSecret = passwordSecret;
+    }
+
+    public String getAuthenticationType() {
+        return authenticationType;
+    }
+
+    public void setAuthenticationType(String authenticationType) {
+        this.authenticationType = authenticationType;
+    }
+
+    public String getHeaderKey() {
+        return headerKey;
+    }
+
+    public void setHeaderKey(String headerKey) {
+        this.headerKey = headerKey;
+    }
+
+    public Long getCredit() {
+        return credit;
+    }
+
+    public void setCredit(Long credit) {
+        this.credit = credit;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     @Override()
