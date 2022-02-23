0cbb15be7c08c52331d6642b716462a26d5c3c9b
meveo.admin <no-mail@meveo.com>
2022-02-23 10:07:09 +0000

    Add the cet java source file : Credential.javain the module : mv-credentials

--- facets/java/org/meveo/model/customEntities/Credential.java
+++ facets/java/org/meveo/model/customEntities/Credential.java
@@ -19,6 +19,8 @@ public class Credential implements CustomEntity {
     @JsonIgnore()
     private DBStorageType storages;
 
+    private String headerValue;
+
     @Override()
     public String getUuid() {
         return uuid;
@@ -34,6 +36,14 @@ public class Credential implements CustomEntity {
 
     public void setStorages(DBStorageType storages) {
         this.storages = storages;
+    }
+
+    public String getHeaderValue() {
+        return headerValue;
+    }
+
+    public void setHeaderValue(String headerValue) {
+        this.headerValue = headerValue;
     }
 
     @Override()
